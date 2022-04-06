a2c4b0de434217a8976d7e0136eca3cd293f14b7
rolisangor <siliiura@gmail.com>
2022-04-06 14:28:25 +0200

    delete account service

--- account-service/src/main/java/com/managementsystem/accountservice/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.managementsystem.accountservice.repository;
-
-import com.managementsystem.accountservice.model.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<Account, Long> {
-
-}
